56677c2d08228ddc7d1f552efe77fd9009940480
Siddharth Bhardwaj <siddharth24553@iiitd.ac.in>
2025-02-19 04:38:00 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -155,8 +155,8 @@ def encode_b_type(instruction, rd, rs1, val, label, counter):
     funct3 = b_type_instructions[instruction]["funct3"]
     opcode = b_type_instructions[instruction]["opcode"]
 
-    rd_bin = register[rd]
-    rs1_bin = register[rs1]
+    rd= register[rd]
+    rs1= register[rs1]
 
     if val in label:
         imm = (to_twos_complement(int(label[val]) - int(counter), 13))
@@ -168,7 +168,7 @@ def encode_b_type(instruction, rd, rs1, val, label, counter):
     imm_4_1 = imm[7:11]
     imm_11 = imm[11]
 
-    binary_instruction = imm_12 + imm_10_5 + rs1_bin + rd_bin + funct3 + imm_4_1 + imm_11 + opcode
+    binary_instruction = imm_12 + imm_10_5 + rs1 + rd + funct3 + imm_4_1 + imm_11 + opcode
     
     return binary_instruction
 
